793e02a65a734b371affe8e6f5ba17fa70b2a2cf
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-01 20:36:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   document.querySelector("#text").textContent = "This is really cool!"
+    console.dir(document.querySelector("#text"))
+
+    
+
+    })
+ 
+  
+
+  
